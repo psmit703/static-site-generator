504f99cef890d889ee1f5685d748fdbfcbb5b8be
Pete Smith <105141656+psmit703@users.noreply.github.com>
2024-03-17 02:28:07 -0400

    made replaceREFS() more modular, also minor formatting changes with comments

--- script.py
+++ script.py
@@ -1,47 +1,51 @@
-# program type:
+# Static Site Generator
 # JSON -> HTML -> list(HTML)
 
 import json
 
-# replaces all REFs ("$REF-...") in siteData JSON/dict with the actual data from other JSON files
-# recursive function
-# dict() | list() -> dict() | list() | str()
-
 
 def replaceREFS(siteData):
+    # replaces all REFs ("$REF-...") in siteData JSON/dict with the actual data from other JSON files
+    # recursive function
+    # dict() | list() -> dict() | list() | str()
+
     if type(siteData) == dict:
-        for key in siteData:
-            if type(siteData[key]) == str:
-                if siteData[key].startswith("$REF-"):
-                    with open(siteData[key][5:], "r") as file:
-                        siteData[key] = json.loads(file.read())
-                else:
-                    siteData[key] = replaceREFS(siteData[key])
-            siteData[key] = replaceREFS(siteData[key])
+        for i in siteData:
+            replaceREFSAux(siteData, i)
         return siteData
     elif type(siteData) == list:
         for i in range(len(siteData)):
-            if type(siteData[i]) == str:
-                if siteData[i].startswith("$REF-"):
-                    with open(siteData[i][5:], "r") as file:
-                        siteData[i] = json.loads(file.read())
-                else:
-                    siteData[i] = replaceREFS(siteData[i])
-            siteData[i] = replaceREFS(siteData[i])
+            replaceREFSAux(siteData, i)
         return siteData
     else:
         return siteData
 
 
-# void -> void
+def replaceREFSAux(siteData, i):
+    # handles repetitive pattern previously in replaceREFS()
+    # dict() | list() -> int -> void
+
+    if type(siteData[i]) == str:
+        if siteData[i].startswith("$REF-"):
+            with open(siteData[i][5:], "r") as file:
+                siteData[i] = json.loads(file.read())
+        else:
+            siteData[i] = replaceREFS(siteData[i])
+    siteData[i] = replaceREFS(siteData[i])
 
 
 def main():
+    # main function
+    # void -> void
+
     with open("site.json", "r") as file:
         siteJSON = json.loads(file.read())
 
     replaceREFS(siteJSON)
+    print(json.dumps(siteJSON, indent=4))
 
 
 if __name__ == "__main__":
+    # program entry point
+
     main()
